214ebd5e8f4da16037504afccd5a403b1723f31d
rcardineaud44 <rcardineaud@gmail.com>
2018-11-26 15:33:36 +0100

    Ajout Voiture

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VoitureRepository")
+ */
+class Voiture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Reference;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $kilometrage;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->Reference;
+    }
+
+    public function setReference(string $Reference): self
+    {
+        $this->Reference = $Reference;
+
+        return $this;
+    }
+
+    public function getKilometrage(): ?int
+    {
+        return $this->kilometrage;
+    }
+
+    public function setKilometrage(int $kilometrage): self
+    {
+        $this->kilometrage = $kilometrage;
+
+        return $this;
+    }
+}
