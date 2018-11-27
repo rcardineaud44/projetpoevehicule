d5585197ad5d79421d0d1da29964e797457a25ac
rcardineaud44 <rcardineaud@gmail.com>
2018-11-27 12:24:29 +0100

    Ajout de la reservation

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Voiture;
 use App\Form\AjoutVoitureType;
+use App\Form\ReservationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
@@ -43,4 +44,33 @@ class VoitureController extends Controller
             'form' => $form->createView()
             ]);
     }
+
+    /**
+     * @Route("/reservation/{id_voiture}", name="reservation")
+     */
+    public function reservation($id_voiture, Request $request, EntityManagerInterface $em)
+    {
+        $detailRepo = $em->getRepository(Voiture::class);
+        $Detailvoiture = $detailRepo->find($id_voiture);
+
+        $formResa = $this->createForm(ReservationType::class, $Detailvoiture);
+
+        $formResa->handleRequest($request);
+
+        if($formResa->isSubmitted() && $formResa->isValid()){
+            $Detailvoiture->setDisponibilite(false);
+
+            $em->persist($Detailvoiture);
+            $em->flush();
+
+            $this->addFlash('success', 'Voiture reservée');
+
+            return $this->redirectToRoute('list');
+        }
+
+
+        return $this->render('voiture/reservation.html.twig', [
+            'form' => $formResa->createView(), 'voiture' => $Detailvoiture]);
+    }
+
 }
---
+++ src/Form/ReservationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Voiture;
+use Doctrine\DBAL\Types\BooleanType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+//        $builder
+////            ->add('Disponibilite', HiddenType::class)
+//        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Voiture::class,
+        ]);
+    }
+}
